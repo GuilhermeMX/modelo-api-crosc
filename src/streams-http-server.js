281f446d7f4caa97812445ef722995f4d9084de8
Guilherme M. Xavier <guilhermenen15@gmail.com>
2023-05-29 17:44:42 -0300

    update

--- src/streams-http-server.js
+++ src/streams-http-server.js
@@ -1,7 +1,18 @@
 import http from 'node:http';
+import { Transform } from 'node:stream'
+
+class InvertNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+      const transformed = Number(chunk.toString()) * -1
+      
+      callback(null, Buffer.from(String(transformed)))
+    }
+}
 
 const server = http.createServer((req, res) => {
-
+  return req
+    .pipe(new InvertNumberStream())
+    .pipe(res)
 })
 
 server.listen(3334);
