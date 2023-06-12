ba65dd9e93de50ff1ebebd180cc6bb87320934c8
Guilherme M. Xavier <guilhermenen15@gmail.com>
2023-06-12 15:41:06 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,62 +1,100 @@
-const express = require('express');
-const { v4: uuidv4 } = require("uuid");
+import http from 'node:http'
 
-const app = express();
+// const express = require('express');
+// const { v4: uuidv4 } = require("uuid");
 
-app.use(express.json());
+// const app = express();
+
+// app.use(express.json());
 
 const users = [];
 
-app.post("/user", (request, response) => {
-  const { username, name } = request.body;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
 
-  const userAlreadyExists = users.some(
-    (user) => user.username === username); 
+  const buffers = [];
 
-  //Retornando status se o usuário já existir:   
-  if (userAlreadyExists) {
-    return response.status(400).json({error: "Este usuário já existe!"});
+  for await (const chunk of req) {
+    buffers.push(chunk)
   }
 
-  users.push({
-    username,
-    name,
-    id: uuidv4(),
-    todos: [],
-  });
-
-  return response.status(201).send();
-})
-
-app.get("/todos", (request, response) => {
-  const { username } = request.headers;
-
-  const user = users.find(user => user.username === username);
-
-  if (!user) {
-    return response.status(400).json({ error: "usuário não encontrado" })
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
   }
 
-  return response.json(user.todos)
-})
-
-app.post("/newtodo", (request, response) => {
-  const { title, deadline } = request.body; 
-
-  const { user } = request;
-
-  const todoOperation = {
-    id: uuidv4(),
-    title: '',
-    done: false, 
-    deadline: new Date(deadline), 
-	  created_at: new Date()
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
-  user.todos.push(todoOperation);
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
 
-  return response.status(201).send();
+    users.push({
+      id: 1,
+      name,
+      email,
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end();
 })
 
-app.listen(3333);
+// app.post("/users", (request, response) => {
+//   const { username, name } = request.body;
+
+//   const userAlreadyExists = users.some(
+//     (user) => user.username === username); 
+
+//   //Retornando status se o usuário já existir:   
+//   if (userAlreadyExists) {
+//     return response.status(400).json({error: "Este usuário já existe!"});
+//   }
+
+//   users.push({
+//     username,
+//     name,
+//     id: uuidv4(),
+//     todos: [],
+//   });
+
+//   return response.status(201).send();
+// })
+
+// app.get("/todos", (request, response) => {
+//   const { username } = request.headers;
+
+//   const user = users.find(user => user.username === username);
+
+//   if (!user) {
+//     return response.status(400).json({ error: "usuário não encontrado" })
+//   }
+
+//   return response.json(user.todos)
+// })
+
+// app.post("/newtodo", (request, response) => {
+//   const { title, deadline } = request.body; 
+
+//   const { user } = request;
+
+//   const todoOperation = {
+//     id: uuidv4(),
+//     title: '',
+//     done: false, 
+//     deadline: new Date(deadline), 
+// 	  created_at: new Date()
+//   }
+
+//   user.todos.push(todoOperation);
+
+//   return response.status(201).send();
+// })
+
+server.listen(3333);
 
